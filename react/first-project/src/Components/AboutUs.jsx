b6c2966cf24cb1fedef59a6377a547842b08a9cd
sandeep <sandeep@gmail.com>
2024-10-01 18:53:29 +0530

    changes

---
+++ react/first-project/src/Components/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Common/Header'
+import Footer from './Common/Footer'
+
+export default function AboutUs() {
+  return (
+    <>
+    {/* <Header/> */}
+    <div>
+      About Us
+    </div>
+    {/* <Footer></Footer> */}
+    </>
+  )
+}
---
+++ react/first-project/src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Home from './Components/Home'
+import AboutUs from './Components/AboutUs'
+import Header from './Components/Common/Header'
+import Footer from './Components/Common/Footer'
+import ContactUs from './Components/ContactUs'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Home/>
+    <Header/>
+    {/* <AboutUs/> */}
+    {/* <ContactUs/> */}
+    <Footer/>
+  </StrictMode>,
+)
