dcc118be2eb4302bef7a2009d5b1fa05f3043223
sandeep <sandeep@gmail.com>
2024-10-03 18:39:31 +0530

    changes

--- react/first-project/src/main.jsx
+++ react/first-project/src/main.jsx
@@ -5,14 +5,15 @@ import AboutUs from './Components/AboutUs'
 import Header from './Components/Common/Header'
 import Footer from './Components/Common/Footer'
 import ContactUs from './Components/ContactUs'
+import "./assets/css/index.css"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Home/>
-    <Header/>
+    {/* <Header/> */}
     {/* <AboutUs/> */}
     {/* <ContactUs/> */}
-    <Footer/>
+    {/* <Footer/> */}
   </StrictMode>,
 )
