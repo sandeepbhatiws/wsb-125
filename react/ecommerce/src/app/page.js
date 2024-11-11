0fbbab0cab22efe141ae0880c0c2d21e6f0fc1be
sandeep <sandeep@gmail.com>
2024-11-11 17:12:39 +0530

    changes

---
+++ react/ecommerce/src/app/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Banner from "./Components/Home/Banner";
+
+export default function Home() {
+  return (
+    <>
+      <Banner/>
+      <section
+      class="container mx-auto my-8 flex flex-col justify-center gap-3 lg:flex-row"
+    >
+      
+
+      
+    </section>
+    </>
+  );
+}
