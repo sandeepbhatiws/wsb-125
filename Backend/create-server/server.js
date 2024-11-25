5846ca8eed55f18cf6dd023f28d140aaadb70527
pradeeprai1990 <pradeep.9997@gmail.com>
2024-11-25 18:53:14 +0530

    changes

--- Backend/create-server/server.js
+++ Backend/create-server/server.js
@@ -1,44 +1,29 @@
-const http = require('http');
-const url = require('url');
+const express = require('express');
+const server =  express() // To Create executable
 const apiData = require('./apiData/apiData.js');
 
-http.createServer((request, response) => {
+server.get('/',(request,response) => {
+    response.send('Server is working fine !!');
+})
 
-    const parseUrl = url.parse(request.url,true);
-
-    console.log(parseUrl);
-    if(request.url == '/'){
-        response.end('Server Started !!');
-    } else if(parseUrl.pathname == '/categories' && request.method == 'POST'){
-        
-        if(apiData.categories.length > 0){
-            var data = {
-                status : true,
-                message : 'Record fetch suuccessfully !!',
-                data : apiData.categories
-            }
-        } else {
-            var data = {
-                status : false,
-                message : 'No Record Found !!',
-                data : []
-            }
+server.post('/categories',(request,response) => {
+    if(apiData.categories.length > 0){
+        var data = {
+            status : true,
+            message : 'Record fetch suuccessfully !!',
+            data : apiData.categories
         }
-        
-        response.end(JSON.stringify(data));
     } else {
-        response.end('404 page not found !!');
+        var data = {
+            status : false,
+            message : 'No Record Found !!',
+            data : []
+        }
     }
 
-    // var data = {
-    //     status : true,
-    //     message : 'Record fetch suuccessfully !!',
-    //     data : []
-    // }
+    response.send(data);
+})
 
-    // response.end(JSON.stringify(data));
-    // response.end('<h1>Welcome to WsCubeTech</h1>');
-
-}).listen('8080',() => {
-    console.log('Server is working fine.');
+server.listen(5000,() => {
+    console.log('Server is started');
 });
