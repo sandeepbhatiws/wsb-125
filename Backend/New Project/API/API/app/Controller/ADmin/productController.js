3d25788052799978dd8448578501198ccaf709ea
pradeeprai1990 <pradeep.9997@gmail.com>
2025-01-14 17:03:45 +0530

    Chanegs

--- Backend/New Project/API/API/app/Controller/ADmin/productController.js
+++ Backend/New Project/API/API/app/Controller/ADmin/productController.js
@@ -62,6 +62,133 @@ exports.insert = async (request, response) => {
     })
 }
 
+// exports.index = async (request, response) => {
+
+//     var page = (request.body.page == undefined || request.body.page < 1) ? 1 : request.body.page;
+
+//     var limit = (request.body.limit == undefined) ? 5 : request.body.limit;
+
+//     var skip = (page - 1) * limit;
+
+//     var condition = {
+//         deleted_at:null,
+//     }
+
+//     if (request.body.category_id == undefined) {
+//     } else {
+//         condition.category_id = request.body.category_id
+//     }
+
+//     if (request.body.sub_category_id == undefined) {
+//     } else {
+//         condition.category_id = request.body.sub_category_id
+//     }
+
+//     if (request.body.size_id == undefined) {
+//     } else {
+//         condition.size_id = request.body.size_id
+//     }
+
+//     if (request.body.color_id == undefined) {
+//     } else {
+//         condition.color_id = request.body.color_id
+//     }
+
+//     if (request.body.name == undefined) {
+//     } else {
+//         condition.name = { $regex: request.body.name, $options: 'i' }
+//     }
+
+//     if (request.body.status == undefined) { 
+//     } else {
+//         condition.status = request.body.status
+//     }
+
+
+//     var totalRecords = await productModel.find().countDocuments();
+
+//     // var totalProducts = await productModel.aggregate(
+//     //     [
+//     //         {
+//     //             $count : 'totalRecords'
+//     //         },
+//     //     ]
+//     // )
+
+//     var totalProducts = await productModel.aggregate(
+//         [
+//             {
+//                 $group:{
+//                     _id: "",
+//                     minPrice: { $min: "$sale_price" },
+//                     maxPrice: { $max: "$sale_price" },
+//                     avgPrice: { $avg: "$sale_price" },
+//                     sumPrice: { $sum: "$sale_price" }
+//                 }
+//             }
+//         ]
+//      )
+
+    
+//      var filter = {};
+
+//     if(request.body.color_id || request.body.category_id){
+//         var filter = {
+//             $or: [
+//                 {
+//                     color_id : request.body.color_id
+//                 },
+//                 {
+//                     category_id : request.body.category_id
+//                 }
+//             ]
+//          }
+//     }
+    
+//     await productModel.find(filter)
+//         .select('name category_id sub_category_id color_id size_id actual_price sale_price status order')
+//         .populate('category_id', 'name')
+//         .populate('sub_category_id', 'name')
+//         .populate('color_id', 'name')  
+//         // .populate('size_id', 'name')
+//         .populate('size_ids', 'name')
+//         .limit(limit).skip(skip)
+//         .sort({ _id: 'desc' })
+//         .then((result) => {
+//             if (result.length > 0) {
+//                 const resp = {
+//                     status: true,
+//                     message: 'Record found succesfully',
+//                     totalRecords : totalProducts,
+//                     data: result
+//                 }
+//                 response.send(resp)
+//             }
+//             else {
+//                 const data = {
+//                     status: false,
+//                     message: 'No record  found ',
+//                     data: []
+//                 }
+//                 response.send(data)
+//             }
+//         })
+//         .catch((error) => {
+//             var errormessage = []
+
+//             for (var value in error.errors) {
+//                 console.log(value);
+//                 errormessage.push(error.errors[value].message)
+//             }
+//             let res = {
+//                 status: false,
+//                 message: 'something went wrong',
+//                 data: errormessage
+//             }
+//             response.send(res)
+//         })
+// }
+
 exports.index = async (request, response) => {
 
     var page = (request.body.page == undefined || request.body.page < 1) ? 1 : request.body.page;
@@ -70,50 +197,73 @@ exports.index = async (request, response) => {
 
     var skip = (page - 1) * limit;
 
-    var condition = {
-        deleted_at:null,
+    const addCondition = [
+        {
+            deleted_at : null, 
+        }
+    ];
+    
+    const orCondition = [];
+
+    if (request.body.name == undefined || request.body.name == '') {
+    } else {
+        orCondition.push({ 
+            name : {
+                $regex: request.body.name, $options: 'i' 
+            }
+        })
     }
 
-    if (request.body.category_id == undefined) {
+    if (request.body.category_id == undefined || request.body.category_id == '') {
     } else {
-        condition.category_id = request.body.category_id
+        orCondition.push({ 
+            category_id : request.body.category_id
+        })
     }
 
-    if (request.body.sub_category_id == undefined) {
+
+    if(addCondition.length > 0){
+        var condition = { $and : addCondition }
     } else {
-        condition.category_id = request.body.sub_category_id
+        var condition = {}
+    }
+    
+    if(orCondition.length > 0){
+        condition.$or = orCondition;
     }
 
-    if (request.body.size_id == undefined) {
-    } else {
-        condition.size_id = request.body.size_id
-    }
 
-    if (request.body.color_id == undefined) {
-    } else {
-        condition.color_id = request.body.color_id
-    }
 
-    if (request.body.name == undefined) {
-    } else {
-        condition.name = { $regex: request.body.name, $options: 'i' }
-    }
 
-    if (request.body.status == undefined) { 
-    } else {
-        condition.status = request.body.status
-    }
+    // if (request.body.category_id == undefined) {
+    // } else {
+    //     condition.category_id = request.body.category_id
+    // }
+
+    // if (request.body.sub_category_id == undefined) {
+    // } else {
+    //     condition.category_id = request.body.sub_category_id
+    // }
+
+    // if (request.body.size_id == undefined) {
+    // } else {
+    //     condition.size_id = request.body.size_id
+    // }
+
+    // if (request.body.color_id == undefined) {
+    // } else {
+    //     condition.color_id = request.body.color_id
+    // }
+
+    
+
+    // if (request.body.status == undefined) { 
+    // } else {
+    //     condition.status = request.body.status
+    // }
 
 
     var totalRecords = await productModel.find().countDocuments();
-
-    // var totalProducts = await productModel.aggregate(
-    //     [
-    //         {
-    //             $count : 'totalRecords'
-    //         },
-    //     ]
-    // )
 
     var totalProducts = await productModel.aggregate(
         [
@@ -127,8 +277,8 @@ exports.index = async (request, response) => {
                 }
             }
         ]
-     )
-
+    )
+    
     await productModel.find(condition)
         .select('name category_id sub_category_id color_id size_id actual_price sale_price status order')
         .populate('category_id', 'name')
